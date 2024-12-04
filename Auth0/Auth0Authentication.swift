93d3bc3a6e0969a7f5a469319d4b93e7b8814c9d
EIhab Hejazi <eihab.hejazi@hs-osnabrueck.de>
2024-12-04 10:21:59 +0100

    adapte func token to hs os openid-configuration

--- Auth0/Auth0Authentication.swift
+++ Auth0/Auth0Authentication.swift
@@ -348,7 +348,7 @@ private extension Auth0Authentication {
         let payload: [String: Any] = [
             "client_id": self.clientId
         ]
-        let token = URL(string: "oauth/token", relativeTo: self.url)!
+        let token = URL(string: "/token", relativeTo: self.url)!
         return Request(session: session,
                        url: token,
                        method: "POST",
